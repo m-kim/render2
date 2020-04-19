1424f41097e45315490432c425bd531e7b9381a7
Mark Kim <mbk@cs.utah.edu>
2020-04-18 20:09:54 -0400

    buffering...

--- HelloTriangleApplication.h
+++ HelloTriangleApplication.h
@@ -124,7 +124,10 @@ private:
   void createCommandBuffers();
   void createSyncObjects();
   void createVertexBuffer();
-    
+  void createBuffer(VkDeviceSize, VkBufferUsageFlags, VkMemoryPropertyFlags, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
+
+  void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
+
   uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
 
   void drawFrame();
